049b65e6bbd3d16380686d190eccd7fa096440c5
Zhanghz <466750218@qq.com>
2023-04-26 22:34:33 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.atguigu.git;
+
+/**
+ * @Auther:Zhanghz
+ * @Date: 2023/4/26 - 04 - 26 - 20:32
+ * @Description: com.atguigu.git
+ * @version: 1.0
+ */
+public class GitTest {
+    //这是一个main方法，是程序的入口
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
